112d5c2dbc4c93658825fc3b4462a3e1a69d1f9a
Ubuntu <ubuntu@ip-172-31-22-227.us-west-2.compute.internal>
2022-01-04 04:10:31 +0000

    Refactor highlighting into standalone class.

---
+++ classes/SphinxTextSnippet.php
@@ -0,0 +1,85 @@
+<?php
+
+
+class SphinxTextSnippet {
+
+	private $index;
+
+	private $options;
+
+	private $contextLines = 2;
+
+	private $contextChars = 75;
+
+	private $beforeMatch = "(searchmatch)";
+
+	private $afterMatch = "(/searchmatch)";
+
+	private $chunkSeparator = "...";
+
+
+
+
+	public function __construct($index, $options = array()) {
+
+		$this->index = $index;
+
+		$this->options = count($options) === 0 ? array(
+			"before_match" => $this->beforeMatch,
+			"after_match" => $this->afterMatch,
+			"chunk_separator" => $this->chunkSeparator,
+			"limit" => ($this->contextLines * $this->contextChars),
+			"around" => $this->contextchars,
+		) : $options;
+
+		// list( $contextlines, $contextchars ) = SphinxSearchEngine::userHighlightPrefs( self::$wgUser );
+	}
+
+
+
+	public function setClient($client) {
+		$this->client = $client;
+	}
+
+
+
+	public function getExcerpts($text, $terms, $index = null) {
+
+		$excerpts = $this->client->BuildExcerpts(
+			array( $text ),
+			$this->index,
+			join( ' ', $terms ),
+			$this->options
+		);
+
+		if ( is_array( $excerpts ) ) {
+			$ret = '';
+			foreach ( $excerpts as $entry ) {
+				// remove some wiki markup
+				$entry = preg_replace(
+					'/([\[\]\{\}\*\#\|\!]+|==+|<br ?\/?>)/',
+					' ',
+					$entry
+				);
+				$entry = str_replace(
+					array("<", ">"),
+					array("&lt;", "&gt;"),
+					$entry
+				);
+				$entry = str_replace(
+					array( "(searchmatch)", "(/searchmatch)" ),
+					array( "<span class='searchmatch'>", "</span>" ),
+					$entry
+				);
+				$ret .= "<div style='margin: 0.2em 1em 0.2em 1em;'>$entry</div>\n";
+			}
+		} else {
+			$ret = wfMsg( 'internalerror_info', $this->client->GetLastError() );
+		}
+		
+		return $ret;
+	}
+
+
+
+}
